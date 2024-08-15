d42cc220ecdb283229c5d3a55fa141c66c9518ef
Dennis Muoki <muokid3@gmail.com>
2024-08-15 20:21:41 +0300

    wrap up homescreen, pending navigation

--- app/src/main/java/com/dm/berxley/newsapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/dm/berxley/newsapp/presentation/home/HomeScreen.kt
@@ -1,30 +1,40 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.dm.berxley.newsapp.presentation.home
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dm.berxley.newsapp.R
 import com.dm.berxley.newsapp.presentation.Dimens.ExtraSmallPadding
 import com.dm.berxley.newsapp.presentation.Dimens.ExtraSmallPadding2
 import com.dm.berxley.newsapp.presentation.Dimens.MediumPadding1
 import com.dm.berxley.newsapp.presentation.common.ArticleCardShimmerEffect
+import com.dm.berxley.newsapp.presentation.common.SearchBar
 import com.dm.berxley.newsapp.presentation.home.components.ArticleCard
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen() {
     val newsViewModel = hiltViewModel<HomeViewModel>()
@@ -40,6 +50,21 @@ fun HomeScreen() {
                     .width(150.dp)
                     .height(30.dp)
                     .padding(horizontal = MediumPadding1)
+            )
+
+            Spacer(modifier = Modifier.height(MediumPadding1))
+
+            SearchBar(modifier = Modifier.padding(horizontal = MediumPadding1), text = "", readOnly = true, onValueChange = {}, onClick = {}, onSearch = {})
+
+            Spacer(modifier = Modifier.height(MediumPadding1))
+
+            Text(
+                text = "Headlines here",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = MediumPadding1)
+                    .basicMarquee(), fontSize = 12.sp,
+                color = colorResource(id = R.color.placeholder)
             )
 
             Spacer(modifier = Modifier.height(MediumPadding1))
@@ -72,8 +97,6 @@ fun HomeScreen() {
             }
 
         }
-
-
 
 
     }
