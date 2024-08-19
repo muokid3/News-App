7ea2d818013fa99b95aaeecad2015c800ab3ad18
Dennis Muoki <muokid3@gmail.com>
2024-08-19 12:11:58 +0300

    details screen

--- app/src/main/java/com/dm/berxley/newsapp/presentation/Dimens.kt
+++ app/src/main/java/com/dm/berxley/newsapp/presentation/Dimens.kt
@@ -12,5 +12,6 @@ object Dimens {
     val ExtraSmallPadding2 = 6.dp
     val SmallIconSize = 11.dp
     val IconSize = 20.dp
+    val ArticleImageHeight = 250.dp
 
 }
---
+++ app/src/main/java/com/dm/berxley/newsapp/presentation/details/components/DetailsTopBar.kt
@@ -0,0 +1,75 @@
+package com.dm.berxley.newsapp.presentation.details.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Language
+import androidx.compose.material.icons.filled.NetworkWifi3Bar
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.outlined.BookmarkBorder
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.dm.berxley.newsapp.R
+import com.dm.berxley.newsapp.ui.theme.NewsAppTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailsTopBar(
+    onBrowsingClick: () -> Unit,
+    onShareClick: () -> Unit,
+    onBookmarkClick: () -> Unit,
+    onBackClick: () -> Unit
+) {
+
+    TopAppBar(
+        title = { /*TODO*/ },
+        modifier = Modifier.fillMaxWidth(),
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            actionIconContentColor = colorResource(id = R.color.body),
+            navigationIconContentColor = colorResource(id = R.color.body)
+
+        ),
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+            }
+        },
+         actions = {
+             IconButton(onClick = onBookmarkClick) {
+                 Icon(imageVector = Icons.Outlined.BookmarkBorder, contentDescription = null)
+             }
+             IconButton(onClick = onShareClick) {
+                 Icon(imageVector = Icons.Default.Share, contentDescription = null)
+             }
+             IconButton(onClick = onBrowsingClick) {
+                 Icon(imageVector = Icons.Default.Language, contentDescription = null)
+             }
+
+         }
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPrev() {
+    NewsAppTheme {
+        DetailsTopBar(
+            onBrowsingClick = { /*TODO*/ },
+            onShareClick = { /*TODO*/ },
+            onBookmarkClick = { /*TODO*/ }) {
+
+        }
+    }
+}
